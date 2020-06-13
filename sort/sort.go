db00bd61351b5b99fd8ba65c5ba5b6abd2fb1de9
selimslab <hselimozturk@gmail.com>
2020-06-13 18:48:21 +0300

    add sort by length

---
+++ sort/sort.go
@@ -0,0 +1,22 @@
+package gondor
+
+import (
+	"sort"
+)
+
+type byLength []string
+
+func (s byLength) Len() int {
+	return len(s)
+}
+func (s byLength) Swap(i, j int) {
+	s[i], s[j] = s[j], s[i]
+}
+func (s byLength) Less(i, j int) bool {
+	return len(s[i]) < len(s[j])
+}
+
+func ByLength(s []string) []string{
+	sort.Sort(byLength(s))
+	return s
+}
---
+++ sort/sort_test.go
@@ -0,0 +1,22 @@
+package gondor
+
+import (
+	"testing"
+	"reflect"
+)
+
+func TestReverseRunes(t *testing.T) {
+	cases := []struct {
+		in, want []string
+	}{
+		{[]string{"peach", "banana", "kiwi"}, []string{ "kiwi", "peach", "banana"}},
+	}
+	for _, c := range cases {
+		got := ByLength(c.in)
+
+		if !reflect.DeepEqual(got, c.want) {
+			t.Errorf("SortByLength(%q) == %q, want %q", c.in, got, c.want)
+		}
+	}
+}
+
